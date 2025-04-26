d484053c2b8b19449286e88a9eba5be863079d3a
Woraphon Olanwanitchakun <woraphon.olan@gmail.com>
2025-04-26 18:14:47 +0700

    feat: register/login by username

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/datawow')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/datawow'),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
